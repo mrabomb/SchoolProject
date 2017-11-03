7937f3531946461bebe3026b80dee0e109f36bab
Alex Chauncy <makeanewname@gmail.com>
2017-11-03 18:00:32 -0500

    "Adds a really barebones Trader class"

---
+++ src/Trader.py
@@ -0,0 +1,28 @@
+class Buyer:
+    #initialize Buyer object
+    def __init__(self, offerPrice, volumeBTC ):
+        self.success = false
+        self.offerPrice = offerPrice
+        self.volumeBTC = volumeBTC
+
+    #buy the values
+    def purchase():
+        #do whatever and run whatever
+        
+        #return that the purchase was a success
+        return self.success
+
+
+class Seller:
+    #initialize Buyer object
+    def __init__(self, offerPrice, volumeBTC ):
+        self.success = false
+        self.offerPrice = offerPrice
+        self.volumeBTC = volumeBTC
+
+    #buy the values
+    def sell():
+        #do whatever and run whatever
+        
+        #return that the purchase was a success
+        return self.success
